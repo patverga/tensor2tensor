6131a0cc73c896bba90d82a36e7e7db194862240
patverga <pat@cs.umass.edu>
2018-01-03 13:36:04 -0500

    update fb problems

--- tensor2tensor/data_generators/translate_ende.py
+++ tensor2tensor/data_generators/translate_ende.py
@@ -178,28 +178,6 @@ class TranslateUschemaFb15k(translate.TranslateProblem):
     return translate.token_generator(source_path + ".lang1", source_path + ".lang2",
                                      symbolizer_vocab, EOS)
 
-
-  # def generator(self, data_dir, tmp_dir, train):
-  #   tag = "train" if train else "dev"
-  #   root_dir = '/iesl/data/clueweb_2016_full/fb15k_subset/kb/tmp'
-  #   source_filename = '%s.tsv' % tag
-  #   source_path = '%s/%s' % (root_dir, source_filename)
-  #   tmp_fname = os.path.join(tmp_dir, source_filename)
-  #   if not os.path.isfile(tmp_fname):
-  #       copyfile(source_path, tmp_fname)
-  #
-  #   source_vocab = generator_utils.get_or_generate_tabbed_vocab(data_dir, tmp_dir, 'train.tsv',
-  #                                                               index=0,
-  #                                                               vocab_filename=self.vocab_name,
-  #                                                               vocab_size=self.targeted_vocab_size)
-  #   target_vocab = generator_utils.get_or_generate_tabbed_vocab(data_dir, tmp_dir, 'train.tsv',
-  #                                                               index=1,
-  #                                                               vocab_filename=self.vocab_name,
-  #                                                               vocab_size=self.targeted_vocab_size)
-  #   print('Tabbed Generator from %s' % source_path)
-  #   return translate.tabbed_generator(source_path, source_vocab, target_vocab)
-
-
   @property
   def input_space_id(self):
     return problem.SpaceID.EN_TOK
@@ -211,6 +189,84 @@ class TranslateUschemaFb15k(translate.TranslateProblem):
   @property
   def use_subword_tokenizer(self):
     return False
+
+
+@registry.register_problem
+class TranslateUschemaFreebase(TranslateUschemaFb15k):
+  """Problem spec for WMT En-De translation."""
+
+  def __init__(self, was_reversed=False, was_copy=False):
+    super(TranslateUschemaFreebase, self).__init__(was_reversed, was_copy)
+    self.root_dir = '/iesl/data/clueweb_2016_full/freebase_ep_subset/2500k_ep_subset'
+
+  @property
+  def targeted_vocab_size(self):
+    return 513227
+    # return 2**15  # 8192
+
+  @property
+  def vocab_name(self):
+    return 'vocab.uschema_fb'
+
+  @property
+  def source_vocab_name(self):
+    return "%s_source" % self.vocab_name
+
+  @property
+  def target_vocab_name(self):
+    return "%s_target" % self.vocab_name
+
+  def generator(self, data_dir, tmp_dir, train):
+    tag = "train" if train else "dev"
+    source_path = '%s/%s' % (self.root_dir, tag)
+
+    source_vocab_file = '%s/vocab.uschema_fb_source' % self.root_dir
+    target_vocab_file = '%s/vocab.uschema_fb_target' % self.root_dir
+    source_vocab = text_encoder.TokenTextEncoder(source_vocab_file)
+    target_vocab = text_encoder.TokenTextEncoder(target_vocab_file, replace_oov='<UNK>')
+
+    return translate.bi_vocabs_token_generator(source_path + ".lang1", source_path + ".lang2",
+                                               source_vocab, target_vocab, EOS)
+
+  def feature_encoders(self, data_dir):
+
+    source_vocab_filename = os.path.join(self.root_dir, self.source_vocab_name)
+    target_vocab_filename = os.path.join(self.root_dir, self.target_vocab_name)
+    source_token = text_encoder.TokenTextEncoder(source_vocab_filename)
+    target_token = text_encoder.TokenTextEncoder(target_vocab_filename, replace_oov='<UNK>')
+    return {
+      "inputs": source_token,
+      "targets": target_token,
+    }
+
+
+@registry.register_problem
+class TranslateUschemaFreebaseKb(TranslateUschemaFreebase):
+  """Problem spec for WMT En-De translation."""
+
+  def __init__(self, was_reversed=False, was_copy=False):
+    super(TranslateUschemaFreebaseKb, self).__init__(was_reversed, was_copy)
+    self.root_dir = '/iesl/data/clueweb_2016_full/freebase_ep_subset/2500k_ep_subset_with_kb'
+
+  @property
+  def vocab_name(self):
+    return 'vocab.uschema_fb_kb'
+
+
+  def generator(self, data_dir, tmp_dir, train):
+    root_dir = '/iesl/data/clueweb_2016_full/freebase_ep_subset/2500k_ep_subset_with_kb'
+    tag = "train" if train else "dev"
+    source_path = '%s/%s' % (root_dir, tag)
+
+    source_vocab_file = '%s/vocab.uschema_fb_kb_source' % root_dir
+    target_vocab_file = '%s/vocab.uschema_fb_kb_target' % root_dir
+    source_vocab = text_encoder.TokenTextEncoder(source_vocab_file)
+    target_vocab = text_encoder.TokenTextEncoder(target_vocab_file, replace_oov='<UNK>')
+
+    return translate.bi_vocabs_token_generator(source_path + ".lang1", source_path + ".lang2",
+                                               source_vocab, target_vocab, EOS)
+
+
 
 
 
